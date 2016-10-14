05b625c73e304b899964e6f238a7268eb2f381f1
jonnyenglish <j.lf@me.com>
2016-10-14 10:46:07 +0200

    update Twitter Trend

--- Twitter Trends/source.r
+++ Twitter Trends/source.r
@@ -1,8 +1,7 @@
 # Twitter Trends
-# Description: Get recent trends on Twitter
+# Description: See what`s happening on twitter right now
 # Author: Jonathan Langefeld - 2016
 # Icon: http://www.flaticon.com/
-
 # preferences:
 worldwide = %%worldwide%%
 latitude = %%latitude%%
@@ -11,7 +10,6 @@ api_key = '%%api_key%%'
 api_secret = '%%api_secret%%'
 access_token = '%%access_token%%'
 access_secret = '%%access_secret%%'
-
 # load required package
 packages <- function(x) {
   x <- as.character(match.call()[[2]])
@@ -21,21 +19,18 @@ packages <- function(x) {
   }
 }
 packages(twitteR)
-
-# sign in to Twitter
+# sign in to twitter
 origop <- options("httr_oauth_cache")
-options(httr_oauth_cache=TRUE)
+options(httr_oauth_cache=FALSE)
 setup_twitter_oauth(api_key, api_secret, access_token, access_secret)
-options(httr_oauth_cache=origop)
-
-# get woeid
+options(httr_oauth_cache=origop$httr_oauth_cache)
+# check worldwide option
 if (!worldwide) {
-  woeid = closestTrendLocations(48.7, 9.199)$woeid
+  woeid = closestTrendLocations(latitude, longitude)$woeid
 } else {
   woeid = 1
 }
-
-# write result to stream
+#receive data and write to stream
 modelerData = getTrends(woeid)[,c('name','url')]
 var1<-c(fieldName="name",fieldLabel="",fieldStorage="string",fieldMeasure="",fieldFormat="",fieldRole="")
 var2<-c(fieldName="url",fieldLabel="",fieldStorage="string",fieldMeasure="",fieldFormat="",fieldRole="")
